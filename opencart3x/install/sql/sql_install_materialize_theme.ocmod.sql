c179a55cb036802a3a2c846725122610e381291d
Anton Semenov <gatalsky94@yandex.ru>
2017-08-25 21:43:39 +0300

    Nothing important and interesting ¯\(°_o)/¯

---
+++ opencart3x/install/sql/sql_install_materialize_theme.ocmod.sql
@@ -0,0 +1,17 @@
+ALTER TABLE `oc_product` ADD `add_cart` TINYINT(1) DEFAULT 1;
+
+CREATE TABLE IF NOT EXISTS `oc_product_customtab` (
+	`product_customtab_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`product_id` INT(11) NOT NULL,
+	`sort_order` INT(11) NOT NULL,
+	`status` TINYINT(1) NOT NULL,
+	PRIMARY KEY (`product_customtab_id`)
+) ENGINE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `oc_product_customtab_description` (
+	`product_customtab_id` INT(11) NOT NULL,
+	`language_id` INT(11) NOT NULL,
+	`product_id` INT(11) NOT NULL,
+	`title` VARCHAR(255) CHARACTER SET utf8 NOT NULL,
+	`description` TEXT CHARACTER SET utf8 NOT NULL
+) ENGINE=MyISAM;
---
+++ opencart3x/install/sql/sql_uninstall_materialize_theme.ocmod.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `oc_product` DROP `add_cart`;
+DROP TABLE `oc_product_customtab`, `oc_product_customtab_description`;
