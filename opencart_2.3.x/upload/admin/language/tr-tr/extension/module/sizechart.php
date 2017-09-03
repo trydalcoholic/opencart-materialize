a4aa08f1338b1c82ff8ccd34ddf2f2e0934faf51
Anton Semenov <gatalsky94@yandex.ru>
2017-09-03 11:54:03 +0300

    Added support for Turkish. Thank you very much for this Taner İnanır!

---
+++ opencart_2.3.x/upload/admin/language/tr-tr/extension/module/sizechart.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title'] = '<font color="#263238"><b>Materialize Beden Tablosu</b></font>';
+$_['size_chart_title']    = 'Materialize Beden tablosu';
+
+// Text
+$_['text_extension']   = 'Eklentiler';
+$_['text_success']     = 'Başarılı: Beden tablosu modülünü değiştirdiniz!';
+$_['text_edit']        = 'Beden tablosu modülünü düzenle';
+
+// Entry
+$_['entry_status']     = 'Beden Tablosu Durum';
+
+// Error
+$_['error_permission'] = 'UYARI: Beden Tablosu modülünü değiştirme izniniz yok!';
---
+++ opencart_2.3.x/upload/admin/language/tr-tr/extension/theme/materialize.php
@@ -0,0 +1,49 @@
+<?php
+// Heading
+$_['heading_title']                    = 'Materialize Teması';
+
+// Text
+$_['text_theme']                       = 'Temalar';
+$_['text_extension']                   = 'Eklentiler';
+$_['text_success']                     = 'Başarılı: Varsayılan depo temasını değiştirdiniz.!';
+$_['text_edit']                        = 'Varsayılan Mağaza Temasını Düzenle';
+$_['text_general']                     = 'Genel';
+$_['text_product']                     = 'Ürünler';
+$_['text_image']                       = 'Görüntüler';
+
+// Entry
+$_['entry_directory']                  = 'Tema Dizini';
+$_['entry_status']                     = 'Durumu';
+$_['entry_product_limit']              = 'Sayfa Başına Varsayılan Öğe';
+$_['entry_product_description_length'] = 'Liste Açıklama Limiti';
+$_['entry_image_category']             = 'Kategori Görsel Boyutu (G x Y)';
+$_['entry_image_thumb']                = 'Ürünün Küçük Görsel Boyutu (G x Y)';
+$_['entry_image_popup']                = 'Ürün Görüntüsünün Açılır Pencere Boyutu (G x Y)';
+$_['entry_image_product']              = 'Örün Görsel Listesi Boyutu (G x Y)';
+$_['entry_image_additional']           = 'Ek Ürün Görsel Boyutu (G x Y)';
+$_['entry_image_related']              = 'Benzer Ürün Görseli Boyutu (G x Y)';
+$_['entry_image_compare']              = 'Görsel Karşılaştırma Boyutu (G x Y)';
+$_['entry_image_wishlist']             = 'Alışveriş Listesi Görsel Boyutu (G x Y)';
+$_['entry_image_cart']                 = 'Alışveriş Sepeti Görsel Boyutu (G x Y)';
+$_['entry_image_location']             = 'Mağaza Görsel Boyutu (G x Y)';
+$_['entry_width']                      = 'Genişlik';
+$_['entry_height']                     = 'Yükseklik';
+
+// Help
+$_['help_directory'] 	               = 'Bu alan sadece eski temaların yeni tema sistemiyle uyumlu olmasını sağlamak içindir. Burada tanımlanan görüntü boyutu ayarlarında kullanılacak tema dizini ayarlayabilirsiniz.';
+$_['help_product_limit'] 	           = 'Sayfa başına kaç tane katalog öğesi gösterildiğini belirler (ürünler, kategoriler, vb.)';
+$_['help_product_description_length']  = 'Liste görünümünde, kısa açıklama karakter sınırı (kategoriler, özel vs.)';
+
+// Error
+$_['error_permission']                 = 'Uyarı: Varsayılan mağaza temasını değiştirme izniniz yok!';
+$_['error_limit']       	           = 'Ürün Sınırı gerekiyor!';
+$_['error_image_thumb']                = 'Ürün Görseli küçük görsel boyutu gerekli!';
+$_['error_image_popup']                = 'Ürün Görüntüsünün Açılır Pencere Boyutu gerekli!';
+$_['error_image_product']              = 'Ürün Görsel listesi Boyutu gerekli!';
+$_['error_image_category']             = 'Kategori Görsel Listesi Boyutu gerekli!';
+$_['error_image_additional']           = 'Ek Ürün Görsel Boyutu gerekli!';
+$_['error_image_related']              = 'Ek Ürün Görsel Boyutu gerekli!';
+$_['error_image_compare']              = 'Görsel Karşılaştırma Boyutu gerekli!';
+$_['error_image_wishlist']             = 'Alışveriş Listesi Görsel Boyutu gerekli!';
+$_['error_image_cart']                 = 'Alışveriş Sepeti Görsel Boyutu!';
+$_['error_image_location']             = 'Mağaza Görsel Boyutu gerekli!';
