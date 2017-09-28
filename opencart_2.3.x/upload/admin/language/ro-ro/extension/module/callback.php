2c885272f67af2c213f4a85942dc91db486eafdc
Anton Semenov <gatalsky94@yandex.ru>
2017-09-28 13:26:00 +0300

    Updating the Romanian language for the Callback module, thanks to Mitza Dragan! The function exif_imagetype has been replaced.

---
+++ opencart_2.3.x/upload/admin/language/ro-ro/extension/module/callback.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']			= '<font color="#263238"><b>Materialize "Solicitare contact"</b></font>';
+$_['callback_title']		= 'Materialize "Solicitare contact"';
+
+// Text
+$_['text_extension']		= 'Modul';
+$_['text_success']			= 'Setarile moodulului actualizate!';
+$_['text_edit']				= 'Editeaza modulul Materialize "Solicitare contact"';
+
+// Entry
+$_['entry_status']			= 'Status';
+
+// Error
+$_['error_permission']		= 'Nu ai permisiunea sa modifici acest modul!';
---
+++ opencart_2.3.x/upload/catalog/language/ro-ro/mail/callback.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_subject']		= '%s - Comanda solicitare contact';
+$_['text_waiting']		= 'Asteapta un telefon.';
+$_['text_name']			= 'Nume: %s';
+$_['text_telephone']	= 'Telefon: %s';
