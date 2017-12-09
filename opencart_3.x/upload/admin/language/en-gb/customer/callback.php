99b80ace85662d10c3006993495003b91369522d
Anton Semenov <gatalsky94@yandex.ru>
2017-12-09 19:54:40 +0300

    Some changes in language files

--- opencart_3.x/upload/admin/language/en-gb/customer/callback.php
+++ opencart_3.x/upload/admin/language/en-gb/customer/callback.php
@@ -1,7 +1,7 @@
 <?php
 // Heading
 $_['heading_title']			= 'Call Back';
-$_['text_materialize']		= 'If you have problems with the work of the module, please create a theme with a problem on <a href="https://github.com/trydalcoholic/opencart-materialize/issues" target="_blank" rel="noopener"><b>Github</b></a>.<hr><b>Materialize Template</b> is a free theme, if you like the work done and you have the opportunity, please support the development of the project with money via <a href="https://money.yandex.ru/to/41001413377821" target="_blank" rel="noopener"><b>Yandex.Money</b></a> or <a href="https://www.paypal.me/trydalcoholic" target="_blank" rel="noopener"><b>PayPal</b></a>.<hr>You can also help the development of the template, leaving a comment and putting a rating on the <a href="https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=30715" target="_blank" rel="noopener"><b>Marketplace</b></a>.';
+$_['text_materialize']		= 'If you have problems with the work of the module, please create a theme with a problem on <a href="//github.com/trydalcoholic/opencart-materialize/issues" target="_blank" rel="noopener"><b>Github</b></a>.<hr><b>Materialize Template</b> is a free theme, if you like the work done and you have the opportunity, please support the development of the project with money via <a href="//money.yandex.ru/to/41001413377821" target="_blank" rel="noopener"><b>Yandex.Money</b></a> or <a href="//www.paypal.me/trydalcoholic" target="_blank" rel="noopener"><b>PayPal</b></a>.<hr>You can also help the development of the template, leaving a comment and putting a rating on the <a href="//goo.gl/pSwwUR" target="_blank" rel="noopener"><b>Marketplace</b></a>.';
 
 // Text
 $_['text_сalled']			= 'Сalled';
--- opencart_3.x/upload/admin/language/ru-ru/extension/module/materialize.php
+++ opencart_3.x/upload/admin/language/ru-ru/extension/module/materialize.php
@@ -17,7 +17,7 @@ $_['tab_mobile']		= 'Мобильные';
 // Text
 $_['text_extension']	= 'Модули';
 $_['text_success']		= 'Настройки модуля обновлены!';
-$_['text_materialize']	= 'Если у вас возникли проблемы с работой модуля, пожалуйста, создайте тему с проблемой на <a href="://github.com/trydalcoholic/opencart-materialize/issues" target="_blank" rel="noopener"><b>Github</b></a>.<hr><b>Materialize Template</b> — бесплатная тема, если вам нравится проделанная работа и у Вас есть возможность, пожалуйста, поддержите финансово развитие проекта через <a href="://money.yandex.ru/to/41001413377821" target="_blank" rel="noopener"><b>Яндекс.Деньги</b></a> или <a href="://www.paypal.me/trydalcoholic" target="_blank" rel="noopener"><b>PayPal</b></a>.<hr>Также вы можете помочь развитию шаблона, оставив комментарий и поставив оценку на <a href="://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=30715" target="_blank" rel="noopener"><b>Marketplace</b></a>.';
+$_['text_materialize']	= 'Если у вас возникли проблемы с работой модуля, пожалуйста, создайте тему с проблемой на <a href="://github.com/trydalcoholic/opencart-materialize/issues" target="_blank" rel="noopener"><b>Github</b></a>.<hr><b>Materialize Template</b> — бесплатная тема, если вам нравится проделанная работа и у Вас есть возможность, пожалуйста, поддержите финансово развитие проекта через <a href="://money.yandex.ru/to/41001413377821" target="_blank" rel="noopener"><b>Яндекс.Деньги</b></a> или <a href="://www.paypal.me/trydalcoholic" target="_blank" rel="noopener"><b>PayPal</b></a>.<hr>Также вы можете помочь развитию шаблона, оставив комментарий и поставив оценку на <a href="://goo.gl/pSwwUR" target="_blank" rel="noopener"><b>Marketplace</b></a>.';
 
 // Entry
 $_['entry_status']		= 'Статус';
