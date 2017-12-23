63eafa23c9fabbedbc11b44c1e48414499059852
Anton Semenov <gatalsky94@yandex.ru>
2017-12-23 09:46:24 +0300

    Fix #38, Fix #39, Fix #40.

--- opencart_2.3.x/upload/catalog/controller/extension/module/quickorder.php
+++ opencart_2.3.x/upload/catalog/controller/extension/module/quickorder.php
@@ -129,7 +129,7 @@ class ControllerExtensionModuleQuickorder extends Controller {
 			$information_info = $this->model_catalog_information->getInformation($this->config->get('module_quickorder_agreement'));
 
 			if ($information_info) {
-				$data['text_agree'] = sprintf($this->language->get('text_agree'), $this->url->link('information/information/agree', 'information_id=' . $this->config->get('config_account_id'), true), $information_info['title'], $information_info['title']);
+				$data['text_agree'] = sprintf($this->language->get('text_agree'), $this->url->link('information/information/agree', 'information_id=' . $this->config->get('module_callback_agreement'), true), $information_info['title'], $information_info['title']);
 			} else {
 				$data['text_agree'] = '';
 			}
