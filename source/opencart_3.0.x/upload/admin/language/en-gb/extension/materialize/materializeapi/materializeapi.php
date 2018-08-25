4a4d5449e56ea8c1d61f17d107af953fef9bba5d
trydalcoholic <gatalsky94@yandex.ru>
2018-08-25 05:12:00 +0300

    Added Russian translation for Materialize API

--- source/opencart_3.0.x/upload/admin/language/en-gb/extension/materialize/materializeapi/materializeapi.php
+++ source/opencart_3.0.x/upload/admin/language/en-gb/extension/materialize/materializeapi/materializeapi.php
@@ -17,14 +17,14 @@ $_['column_status']				= 'Status';
 
 // Text
 $_['text_total']				= 'Total';
-$_['text_appeal_donate']		= 'Please support the development of the template! <a href="//bit.ly/materializeapi_appeal__review_eng" target="_blank" rel="noopener" class="alert-link">Leave a review</a> about the work or <a href="//bit.ly/materializeapi_appeal__donate_eng" target="_blank" rel="noopener" class="alert-link">donate for development</a>.';
+$_['text_appeal_donate']		= 'Please support the development of the template! <a href="//www.opencart.com/index.php?route=marketplace/extension/info&extension_id=30715" target="_blank" rel="noopener" class="alert-link">Leave a review</a> about the work or <a href="//www.paypal.me/trydalcoholic" target="_blank" rel="noopener" class="alert-link">donate for development</a>.';
 $_['text_donate']				= 'Donate';
-$_['text_translate']			= 'If you want to become a member of the template translation, <a href="//bit.ly/materializeapi_appeal__translate_eng" target="_blank" rel="noopener" class="alert-link">please read the instructions here</a>.';
+$_['text_translate']			= 'If you want to become a member of the template translation, <a href="//github.com/trydalcoholic/opencart-materialize/issues/20" target="_blank" rel="noopener" class="alert-link">please read the instructions here</a>.';
 $_['text_your_version']			= 'Your version';
 $_['text_changes']				= 'Changes';
 $_['text_fixed_bugs']			= 'Fixed bugs';
 
 // Link
-$_['link_paypal']				= '<a href="//bit.ly/materializeapi_appeal__paypal_eng" target="_blank" rel="noopener">PayPal</a>';
-$_['link_yandex']				= '<a href="//bit.ly/materializeapi_appeal__yandex_eng" target="_blank" rel="noopener">Yandex.Money</a>';
-$_['link_translate']			= '<a href="//bit.ly/materializeapi_appeal__translate_eng" target="_blank" rel="noopener" class="btn btn-info">Help</a>';
+$_['link_paypal']				= '<a href="//www.paypal.me/trydalcoholic" target="_blank" rel="noopener">PayPal</a>';
+$_['link_yandex']				= '<a href="//money.yandex.ru/to/41001413377821" target="_blank" rel="noopener">Yandex.Money</a>';
+$_['link_translate']			= '<a href="//github.com/trydalcoholic/opencart-materialize/issues/20" target="_blank" rel="noopener" class="btn btn-info">Help</a>';
