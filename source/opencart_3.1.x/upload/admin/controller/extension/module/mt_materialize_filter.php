8785924104103ca26a721dc146c40e5ee32735e1
trydalcoholic <gatalsky94@yandex.ru>
2018-12-10 07:16:14 +0300

    Changed logic js script filter.

--- source/opencart_3.1.x/upload/admin/controller/extension/module/mt_materialize_filter.php
+++ source/opencart_3.1.x/upload/admin/controller/extension/module/mt_materialize_filter.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package		Materialize Template
- * @author		Anton Semenov
- * @copyright	Copyright (c) 2017 - 2018, Materialize Template. https://github.com/trydalcoholic/opencart-materialize
- * @license		https://github.com/trydalcoholic/opencart-materialize/blob/master/LICENSE
- * @link		https://github.com/trydalcoholic/opencart-materialize
+ * @package     Materialize Template
+ * @author      Anton Semenov
+ * @copyright   Copyright (c) 2017 - 2018, Materialize Template. https://github.com/trydalcoholic/opencart-materialize
+ * @license     https://github.com/trydalcoholic/opencart-materialize/blob/master/LICENSE
+ * @link        https://github.com/trydalcoholic/opencart-materialize
  */
 
 class ControllerExtensionModuleMTMaterializeFilter extends Controller {
--- source/opencart_3.1.x/upload/catalog/view/theme/mt_materialize/js/mt_filter/mt_filter.js
+++ source/opencart_3.1.x/upload/catalog/view/theme/mt_materialize/js/mt_filter/mt_filter.js
@@ -1,102 +1,95 @@
 /**
- * @package		Materialize Template
- * @author		Anton Semenov
- * @copyright	Copyright (c) 2017 - 2018, Materialize Template. https://github.com/trydalcoholic/opencart-materialize
- * @license		https://github.com/trydalcoholic/opencart-materialize/blob/master/LICENSE
- * @link		https://github.com/trydalcoholic/opencart-materialize
+ * @package     Materialize Template
+ * @author      Anton Semenov
+ * @copyright   Copyright (c) 2017 - 2018, Materialize Template. https://github.com/trydalcoholic/opencart-materialize
+ * @license     https://github.com/trydalcoholic/opencart-materialize/blob/master/LICENSE
+ * @link        https://github.com/trydalcoholic/opencart-materialize
  */
 
 (function($) {
 	"use strict";
 
-	$.fn.mtFilter = function(options) {
-		const settings = $.extend({
-			action: '',
-			startFilter: false,
-			productGrid: 's12 m6',
-		}, options);
+	let settings = {
+		grid: 's12 m6',
+		action: '',
+		contentMTFilter: '#content-mt-filter',
+		loader: '<div id="mt-filter-loader" class="loader-wrapper"><div class="showbox"><div class="loader"><svg class="circular" viewBox="25 25 50 50"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/></svg></div></div></div>'
+	};
 
-		return this.each(function() {
-			let timer;
+	const methods = {
+		init: function(options) {
+			settings = $.extend(settings, options);
 
-			if (options) {
-				$.extend(settings, options);
-			}
+			return this.each(function() {
+				let $this = $(this),
+					timer = null,
+					inputs = $this.find('input, select');
 
-			let mtFilter, inputs, contentMTFilter, slider;
-
-			mtFilter = $(this);
-
-			inputs = $(this).find('.materialize-filters__item--filter-content input, .materialize-filters__item--filter-content select').map(function () {
-				return this;
-			});
-
-			contentMTFilter = $('#content-mt-filter');
-			slider = document.getElementById('slider-prices');
-
-			slider.noUiSlider.on('end', function() {
-				startFilter();
-			});
-
-			inputs.change(function() {
-				startFilter();
-			});
-
-			if (options['startFilter'] === true) {
-				startFilter();
-			}
-
-			function startFilter() {
-				clearTimeout(timer);
-
-				timer = setTimeout(function() {
-					let productDisplay, form = mtFilter.serializeArray();
-
-					console.log(form);
-					/*if (localStorage.getItem('display') === 'list') {
-						productDisplay = 'list';
-					} else {
-						productDisplay = 'grid';
+				inputs.change(function() {
+					if (timer) {
+						clearTimeout(timer);
 					}
 
-					$.ajax({
-						url: 'index.php?route=extension/module/mt_materialize_filter/filter' + options['action'] + '&product_grid=' + options['productGrid'] + '&product_display=' + productDisplay,
-						type: 'post',
-						data: form,
-						dataType: 'json',
-						beforeSend: function () {
-							contentMTFilter.css({
-								'opacity': '0.5',
-								'transform': 'translateY(50px)'
-							});
-							contentMTFilter.before('<div class="loader-wrapper"><div class="showbox"><div class="loader"><svg class="circular" viewBox="25 25 50 50"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/></svg></div></div></div>');
-						},
-						success: function (json) {
-							setTimeout(function () {
-								contentMTFilter.css({
-									'opacity': '1',
-									'transform': 'translateY(0)'
-								}).empty();
-								$('.loader-wrapper').remove();
-								contentMTFilter.append(json['products']);
-								setLocation(json['current_location']);
-							}, 300); /!* todo-materialize This delay is added only to test the preloader *!/
+					timer = setTimeout(function() {
+						console.log(inputs.serializeArray());
+						methods.request(inputs.serializeArray(), settings);
+					}, 200);
+				});
+			});
+		},
+		request: function(data, settings) {
+			let productDisplay, contentMtFilter;
 
-							function setLocation(curLoc) {
-								try {
-									history.pushState(null, null, curLoc);
-									return;
-								} catch(e) {}
-
-								location.hash = '#' + curLoc;
-							}
-						},
-						error: function (xhr, ajaxOptions, thrownError) {
-							alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
-						}
-					});*/
-				}, 200);
+			if (localStorage.getItem('display') === 'list') {
+				productDisplay = 'list';
+			} else {
+				productDisplay = 'grid';
 			}
-		});
+
+			contentMtFilter = $(settings['contentMTFilter']);
+
+			return $.ajax({
+				url: 'index.php?route=extension/module/mt_materialize_filter/filter' + settings['action'] + '&grid=' + settings['grid'] + '&product_display=' + productDisplay,
+				type: 'post',
+				data: data,
+				dataType: 'json',
+				beforeSend: function () {
+					contentMtFilter.css({
+						'opacity': '0.5',
+						'transform': 'translateY(50px)'
+					}).before(settings['loader']);
+				},
+				success: function (json) {
+					setTimeout(function () {
+						contentMtFilter.css({
+							'opacity': '1',
+							'transform': 'translateY(0)'
+						}).empty();
+
+						$('#mt-filter-loader').remove();
+
+						contentMtFilter.append(json['products']);
+					}, 300); /* todo-materialize This delay is added only to test the preloader */
+
+					methods.setLocation(json['current_location']);
+				},
+				error: function (xhr, ajaxOptions, thrownError) {
+					alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+				}
+			});
+		},
+		setLocation: function(curLoc) {
+			return history.pushState(null, null, curLoc);
+		}
 	};
+
+	$.fn.mtFilter = function(method) {
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			return console.error('Method ' + method + ' not found');
+		}
+	}
 })(jQuery);
