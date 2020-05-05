a7caecb6446196a72e0c473e4648de6cbc28769f
Semenov Anton <gatalsky94@yandex.ru>
2020-05-05 04:25:50 +0300

    Interactive color change.

--- src/upload/admin/view/javascript/ultimaterial/template/extension/theme/ultimaterial/framework.js
+++ src/upload/admin/view/javascript/ultimaterial/template/extension/theme/ultimaterial/framework.js
@@ -40,24 +40,24 @@ class UFramework {
       this.colorElements[element] = new Pickr({
         el: element,
         useAsButton: true,
-        default: '#fff',
-        theme: 'classic',
+        default: element.value,
+        theme: 'nano',
 
         swatches: [
-          'rgba(244, 67, 54, 1)',
-          'rgba(233, 30, 99, 0.95)',
-          'rgba(156, 39, 176, 0.9)',
-          'rgba(103, 58, 183, 0.85)',
-          'rgba(63, 81, 181, 0.8)',
-          'rgba(33, 150, 243, 0.75)',
-          'rgba(3, 169, 244, 0.7)',
-          'rgba(0, 188, 212, 0.7)',
-          'rgba(0, 150, 136, 0.75)',
-          'rgba(76, 175, 80, 0.8)',
-          'rgba(139, 195, 74, 0.85)',
-          'rgba(205, 220, 57, 0.9)',
-          'rgba(255, 235, 59, 0.95)',
-          'rgba(255, 193, 7, 1)'
+          'rgba(0, 0, 0, 0.4)',
+          'rgba(244, 67, 54, 0.4)',
+          'rgba(233, 30, 99, 0.4)',
+          'rgba(156, 39, 176, 0.4)',
+          'rgba(103, 58, 183, 0.4)',
+          'rgba(63, 81, 181, 0.4)',
+          'rgba(33, 150, 243, 0.4)',
+          'rgba(3, 169, 244, 0.4)',
+          'rgba(0, 188, 212, 0.4)',
+          'rgba(0, 150, 136, 0.4)',
+          'rgba(76, 175, 80, 0.4)',
+          'rgba(139, 195, 74, 0.4)',
+          'rgba(205, 220, 57, 0.4)',
+          'rgba(255, 235, 59, 0.4)'
         ],
 
         components: {
@@ -66,20 +66,37 @@ class UFramework {
           hue: true,
 
           interaction: {
-            hex: true,
-            rgba: true,
-            hsva: true,
             input: true,
+            cancel: true,
             save: true
           }
         }
-      }).on('init', picker => {
-        let value = picker.getSelectedColor().toRGBA().toString(0);
+      }).on('show', () => {
+        this.initColor = element.value;
+      }).on('change', color => {
+        let value = color.toRGBA().toString(0);
 
         element.value = value;
         element.style.setProperty('--color-picker', value);
+
+        let target = document.getElementById(element.dataset.target),
+            dataSet = element.dataset.property;
+
+        target.style.setProperty(dataSet, value);
+      }).on('cancel', () => {
+        let value = this.initColor;
+
+        element.value = value;
+        element.style.setProperty('--color-picker', value);
+
+        let target = document.getElementById(element.dataset.target),
+            dataSet = element.dataset.property;
+
+        target.style.setProperty(dataSet, value);
       }).on('save', color => {
         let value = color.toRGBA().toString(0);
+
+        this.initColor = value;
 
         element.value = value;
         element.style.setProperty('--color-picker', value);
@@ -90,6 +107,18 @@ class UFramework {
         target.style.setProperty(dataSet, value);
 
         this.colorElements[element].hide();
+      }).on('hide', () => {
+        let value = this.initColor;
+
+        element.value = value;
+        element.style.setProperty('--color-picker', value);
+
+        let target = document.getElementById(element.dataset.target),
+          dataSet = element.dataset.property;
+
+        target.style.setProperty(dataSet, value);
+
+        this.initColor = undefined;
       });
     });
   }
